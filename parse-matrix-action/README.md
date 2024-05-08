c7a1dba7a99ed5a24308f42ec5f43bd854b22fed
Justus Dieckmann <justusdieckmann@wwu.de>
2024-05-08 23:22:48 +0200

    First commit

---
+++ parse-matrix-action/README.md
@@ -0,0 +1,9 @@
+# Contribution
+
+After making changes to `index.js`, run 
+```
+ncc build index.js --license licenses.txt
+```
+to build a new `dist/index.js`.
+
+`ncc` can be installed with `npm i -g @vercel/ncc`.
---
+++ parse-matrix-action/index.js
@@ -0,0 +1,54 @@
+const core = require('@actions/core');
+
+try {
+    let {
+        "main-db": mainDb,
+        "main-php": mainPhp,
+        "main-moodle": mainMoodle,
+        "moodle-php": moodlePhp,
+        "dbs": dbs,
+        "moodle-plugin-ci": moodlePluginCi
+    } = JSON.parse(core.getInput('input'));
+
+    mainDb ??= "pgsql";
+    dbs ??= ["pgsql", "mariadb", "mysql", "mssql"];
+
+    core.setOutput("moodle_plugin_ci", moodlePluginCi);
+
+    core.setOutput("static_matrix", JSON.stringify({
+        include: [{
+            "php": mainPhp,
+            "moodle-branch": mainMoodle,
+            "database": mainDb
+        }]
+    }));
+
+    const testmatrix = [];
+
+    for (const [moodle, phps] of Object.entries(moodlePhp)) {
+        for (const php of phps) {
+            testmatrix.push({
+                "php": php,
+                "moodle-branch": moodle,
+                "database": mainDb
+            })
+        }
+    }
+
+    for (const db of dbs) {
+        if (db === mainDb)
+            continue;
+        testmatrix.push({
+            "php": mainPhp,
+            "moodle-branch": mainMoodle,
+            "database": db
+        })
+    }
+
+    core.setOutput("test_matrix", JSON.stringify({
+        include: testmatrix
+    }));
+
+} catch (error) {
+    core.setFailed(error.message);
+}
